f2af9fed33f3cbdf520982d31b4ed3e09f3040c2
GetyeTek <getyeteklu2@gmail.com>
2026-09-02 14:11:40 +0300

    Fix Android 14 FGS crash in OmniSolveService and install dis

--- app/src/main/java/com/omni/hub/OmniApp.kt
+++ app/src/main/java/com/omni/hub/OmniApp.kt
@@ -1,9 +1,19 @@
 package com.omni.hub
 
 import android.app.Application
+import com.omni.hub.api.OmniLogger
 
 class OmniApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        OmniLogger.init(this)
+        OmniLogger.log("APP_INIT", "Omni Hub Application process booted")
+
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            val stackTrace = throwable.stackTraceToString()
+            OmniLogger.log("CRASH_FATAL", "💥 UNCAUGHT CRASH on ${thread.name}: ${throwable.message}\n$stackTrace")
+            defaultHandler?.uncaughtException(thread, throwable)
+        }
     }
 }
